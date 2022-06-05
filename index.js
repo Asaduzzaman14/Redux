0777694de42652a25621029cc911a21f570831aa
Md Asaduzaman <asadasad9282@gmail.com>
2022-06-05 18:05:24 +0600

    reducer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //defining variable
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
-const ADD_USER = 'ADD_USER';
+// const ADD_USER = 'ADD_USER';
 
 
 //state
@@ -9,9 +9,10 @@ const initialCounterState = {
     count: 0
 
 }
-const initialUserState = {
-    users: [{ name: 'asad' }]
-};
+
+// const initialUserState = {
+//     users: [{ name: 'asad' }]
+// };
 
 // action creter - object type, paylode
 const icrementCounter = () => {
@@ -26,9 +27,48 @@ const decrementCounter = () => {
     }
 }
 
-const addUser = () => {
-    return {
-        type: ADD_USER,
-        payload: { name: "rakib" }
+const counter = (state = initialCounterState, action) => {
+    switch (action.type) {
+
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+
+
+
+        default:
+            state
     }
 }
+
+// const addUser = () => {
+//     return {
+//         type: ADD_USER,
+//         payload: { name: "rakib" }
+//     }
+// }
+
+//  create reducer for counter
+
+
+
+
+
+//  1. state
+//  2. dispatch action
+//  3 . reducer
+//  4. store
+
+
+
+
+// ////////////
+
